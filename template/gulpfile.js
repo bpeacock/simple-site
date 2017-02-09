b8a2d2343d06926afc5e21ffb985491021188fc0
Brian Peacock <bpeacock@fastfig.com>
2017-02-09 03:02:11 -0500

    Changed html to jade

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -2,12 +2,12 @@ var gulp  = require('gulp'),
     watch = require('gulp-watch'),
     webserver = require('gulp-webserver');
 
-gulp.task('html',   require('./gulp/html'));
+gulp.task('jade',   require('./gulp/jade'));
 gulp.task('css',    require('./gulp/css'));
 gulp.task('js',     require('./gulp/js'));
 gulp.task('static', require('./gulp/static'));
 
-gulp.task('default', ['html', 'css', 'js', 'static']);
+gulp.task('default', ['jade', 'css', 'js', 'static']);
 
 gulp.task('serve', function() {
   watch(['site/**/*', 'config.js', 'config.less'], function() {
